d42b95f6b50c8683735bbd32766eef5d60ce5faf
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2025-03-09 01:09:56 +0600

    Project Done.

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // UnitaxTour name
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lfgd0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,12 +28,71 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const tourCollection = client.db('tourDB').collection('tour');
+    const destinationCollection = client.db('destinationDB').collection('destination');
+
+    app.get('/tour', async(req,res) =>{
+        const cursor = tourCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+
+    app.get('/tour/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await tourCollection.findOne(query);
+      res.send(result);
+    });
+
+    // destination
+
+    app.get('/destination', async(req,res) =>{
+      const cursor = destinationCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    
+    app.get('/destination/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await destinationCollection.findOne(query);
+      res.send(result);
+    });
+
+
+    app.post('/tour', async(req,res) =>{
+        const newTour = req.body;
+        console.log(newTour);
+        const result = await tourCollection.insertOne(newTour);
+        res.send(result);
+    });
+
+    app.post('/destination', async(req,res) =>{
+      const newDestination = req.body;
+      console.log(newDestination);
+      const result = await destinationCollection.insertOne(newDestination);
+      res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
